d3483350522a46e8e34e7dd743e6db240867027e
Poorya Mohammadi <poorya@gmx.com>
2018-10-23 00:13:33 +0330

    Insertion Sort (Console C++) added

---
+++ CLRS Algorithms/Console C++/Sort Algorithms/Insertion Sort.cpp
@@ -0,0 +1,46 @@
+// This algorithm is on page 18 in clrs Book
+
+// This aligoritm take an unsorted array as input and then sort it .
+
+#include <iostream>
+
+using namespace std;
+
+void Insertion_Sort(int *A,int n);
+
+int main(){
+	int A[100];
+	int b;
+	cout << "Element(s) number : ";
+	cin >> b ;
+	for(int i=0;i<b;i++){
+		cin >> A[i];
+	}
+	system("clear");
+	cout << "Current Array : "<< endl;
+	for(int i=0;i<b;i++){
+		cout << "[ " << A[i] << " ]  ";
+	}
+	Insertion_Sort(A,b);
+	cout <<endl<< "Sorted Array : " << endl;
+	for(int i=0;i<b;i++){
+		cout << "[ " << A[i] << " ]  ";
+	}
+	cout << endl;
+	return 0;
+}
+
+void Insertion_Sort(int *A,int n){
+	int key;
+	int i;
+	for(int j=1;j<n;j++){
+		key = A[j];
+        	 i = j-1;
+		while(i>=0 && A[i] > key){
+			A[i+1]=A[i];
+			i--;
+		}
+		A[i+1]=key;	
+	}
+}
+
