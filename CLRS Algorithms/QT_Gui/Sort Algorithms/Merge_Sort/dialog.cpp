66b8f20f6c8402257dbc60fd0fcc20bd9fc71558
Poorya Mohammadi <poorya@gmx.com>
2018-10-27 00:56:38 +0330

    Two Merge methods (decreasing , increasing) added to project

--- CLRS Algorithms/QT_Gui/Sort Algorithms/Merge_Sort/dialog.cpp
+++ CLRS Algorithms/QT_Gui/Sort Algorithms/Merge_Sort/dialog.cpp
@@ -36,3 +36,116 @@ void Dialog::Add(){
     Arr[in]=dou;
     in++;
 }
+
+
+void Dialog::on_btnSort_clicked()
+{
+    //clicked on sort
+    Sort();
+}
+
+void Dialog::Sort()
+{
+    while(ui->list_sort->count()>0){
+        ui->list_sort->takeItem(0);
+    }
+     if(ui->Chbox_mode->isChecked()==false){
+         //Merge_sort
+     }
+     else{
+         //Merge_sort_dec
+     }
+}
+
+vodi Dialog::Merge(int arr[], int l, int m, int r){
+    int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int *L = new int[n1];
+        int *R = new int[n2];
+
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+}
+
+
+
+void Dialog::Merge_Dec(int arr[],int l , int m,int r){
+    int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int *L = new int[n1];
+        int *R = new int[n2];
+
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] >= R[j])//
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+}
+
