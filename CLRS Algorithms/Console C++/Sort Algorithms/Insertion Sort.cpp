cd549ffa5dd7c7a65cc45f05f698b998bbc4328a
Poorya Mohammadi <poorya@gmx.com>
2018-10-31 02:01:31 +0330

    Insertion sort modidified

--- CLRS Algorithms/Console C++/Sort Algorithms/Insertion Sort.cpp
+++ CLRS Algorithms/Console C++/Sort Algorithms/Insertion Sort.cpp
@@ -1,46 +1,49 @@
-// This algorithm is on page 18 in clrs Book
-
-// This aligoritm take an unsorted array as input and then sort it .
-
 #include <iostream>
 
 using namespace std;
 
-void Insertion_Sort(int *A,int n);
+void Selection_Sort(int *A, int n);
+void swap(int *a, int *b);
 
-int main(){
+int main() {
 	int A[100];
-	int b;
+	int n;
 	cout << "Element(s) number : ";
-	cin >> b ;
-	for(int i=0;i<b;i++){
+	cin >> n;
+	for (int i = 0; i<n; i++) {
 		cin >> A[i];
 	}
-	system("clear");
-	cout << "Current Array : "<< endl;
-	for(int i=0;i<b;i++){
+	//system("clear");
+	cout << "Current Array : " << endl;
+	for (int i = 0; i<n; i++) {
 		cout << "[ " << A[i] << " ]  ";
 	}
-	Insertion_Sort(A,b);
-	cout <<endl<< "Sorted Array : " << endl;
-	for(int i=0;i<b;i++){
+	Selection_Sort(A, n);
+	cout << endl << "Sorted Array : " << endl;
+	for (int i = 0; i<n; i++) {
 		cout << "[ " << A[i] << " ]  ";
 	}
 	cout << endl;
 	return 0;
 }
 
-void Insertion_Sort(int *A,int n){
-	int key;
-	int i;
-	for(int j=1;j<n;j++){
-		key = A[j];
-        	 i = j-1;
-		while(i>=0 && A[i] > key){ //if we change a[i]<key algorithm will be decresing
-			A[i+1]=A[i];
-			i--;
+void Selection_Sort(int *A, int n) {
+	int min;
+	for (int i = 0; i<n; i++) {
+		min = i;
+		for (int j = i+1; j<n; j++) {
+			if (A[j]<A[min]) {
+				min=j;
+			}
 		}
-		A[i+1]=key;	
+		swap(A[min], A[i]);
 	}
+
 }
 
+void swap(int *a, int *b) {
+	int *temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
