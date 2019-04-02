627db9543d2a8943dd74184a98f84adf640cb580
Poorya Mohammadi <poorya@gmx.com>
2019-04-02 05:21:10 +0430

    Bubble sort algorithm with python added to project

---
+++ CLRS Algorithms/Python/Sort Algorithms/Bubble Sort.py
@@ -0,0 +1,43 @@
+#   Poorya Mohammadi Nasab
+#   April,2019
+
+import os
+
+def Bubble_Sort(Array=[],n=0):
+    #begin
+    for i in range(n-1):
+        for j in range(n-i-1):
+            # begin
+            if Array[j] > Array[j+1]:
+                tmp = Array[j]
+                Array[j]=Array[j+1]
+                Array[j+1]=tmp
+            # end
+    return Array
+    #end
+    
+
+# main
+
+Array = []
+while True:
+    # begin
+    try:
+        n = int(input('.:: Element(s) number : '))
+    except :
+        print('Error : You must enter a positive integer !!')
+        continue
+    else :
+        if n<=0:
+            print('Error : You must enter a positive integer !!')
+        else :
+            break
+    # end
+
+for i in range(n):
+    Array.append(float(input('.:: Enter num[ {} ] : '.format(i+1))))
+
+os.system('clear')
+print(Array)
+print(Bubble_Sort(Array,n))
+# end main 
