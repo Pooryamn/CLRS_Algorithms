92775c102c92bbc1ce29fd1392db7ee6e4fae9da
Pooryamn <poorya.m.n.b@gmail.com>
2019-07-26 00:09:28 +0430

    Show method added to quick sort

--- CLRS Algorithms/Console C++/Sort Algorithms/Quick_Sort.cpp
+++ CLRS Algorithms/Console C++/Sort Algorithms/Quick_Sort.cpp
@@ -2,7 +2,42 @@
 
 using namespace std;
 
-int main(){
+void show(int* A,int n);
 
+int main(){
+    
+    system("clear");
+
+    int* A;
+    int n;
+
+    cout << ".:: Element number : ";
+    cin >> n;
+
+    A = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[" << i+1 << "] : ";
+        cin >> A[i];
+    }
+
+    system("clear");
+    cout << ".:: Input Array : ";
+    show(A,n);
+
+    
     return 0;
 }
+
+void show(int* A,int n){
+    cout << "[";
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i];
+        if(i<n-1){
+            cout <<',';
+        }
+    }
+    cout << "]"<< endl;
+}   
