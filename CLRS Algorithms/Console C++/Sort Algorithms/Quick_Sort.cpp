271e7381068325e054a1f7dc0dc9648794e73d60
Pooryamn <poorya.m.n.b@gmail.com>
2019-07-26 00:59:41 +0430

    partition last added to quick sort that sets pivot as last element of array

--- CLRS Algorithms/Console C++/Sort Algorithms/Quick_Sort.cpp
+++ CLRS Algorithms/Console C++/Sort Algorithms/Quick_Sort.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 void show(int* A,int n);
+void Quick_Sort(int* A,int first,int last);
+int partition_last(int* A,int first,int last);
 
 int main(){
     
@@ -26,6 +28,10 @@ int main(){
     cout << ".:: Input Array : ";
     show(A,n);
 
+    Quick_Sort(A,0,n-1);
+    
+    show(A,n);
+
     
     return 0;
 }
@@ -40,4 +46,36 @@ void show(int* A,int n){
         }
     }
     cout << "]"<< endl;
-}   
+} 
+
+void Quick_Sort(int* A,int first,int last){
+    if(first < last){
+        int q = partition_last(A,first,last);
+        Quick_Sort(A,first,q-1);
+        Quick_Sort(A,q+1,last);
+    }
+}
+
+int partition_last(int* A,int first,int last){
+    int x = A[last];
+    int i = first - 1;
+
+    for (int j = first; j < last; j++)
+    {
+        if(A[j] <= x){
+            i++;
+            
+            int tmp = A[j];
+            A[j] = A[i];
+            A[i] = tmp;
+
+
+        }
+    }
+    int tmp = A[i+1];
+    A[i+1] = A[last];
+    A[last] = tmp;
+
+    return i+1;
+    
+}
